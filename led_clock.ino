c177eeb09878365a65aae450930a1ac23f22d3a6
Steven Keyes <stevenrkeyes@gmail.com>
2019-01-16 00:17:03 -0800

    Add use of RTClib
    This commit adds RTClib as a git submodule, and it adds the include to led_clock.ino. It also runs some basic code (copied from the RTClib example) that verifies the device is able to communicate with the DS3231.
    
    This commit also adds code to set up the serial port.

--- led_clock.ino
+++ led_clock.ino
@@ -7,12 +7,18 @@
 */
 
 #include <Adafruit_NeoPixel.h>
+#include "src/RTClib/RTClib.h"
 #include "src/SoftPWM/SoftPWM.h"
 
 #define POWER_LED_ENABLE A0
 #define MANDALA_DATA A2
 
 Adafruit_NeoPixel led_array = Adafruit_NeoPixel(11*12, MANDALA_DATA, NEO_GRB + NEO_KHZ800);
+
+RTC_DS3231 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -23,6 +29,26 @@ void setup() {
   SoftPWMSetPolarity(POWER_LED_ENABLE, SOFTPWM_INVERTED);
   SoftPWMSet(POWER_LED_ENABLE, 0);
   //pinMode(POWER_LED_ENABLE, OUTPUT);
+
+  Serial.begin(9600);
+
+  delay(3000); // wait for console opening
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+  }
+  else {
+    Serial.println("Found RTC");
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, lets set the time!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 
   for (int fadeValue = 0 ; fadeValue <= 130; fadeValue += 10) {
     SoftPWMSet(POWER_LED_ENABLE, fadeValue);
@@ -46,7 +72,7 @@ void setup() {
 
   led_array.begin();
 
-  for (int i=0; i < led_array.numPixels(); i++) {
+  for (int i=0; i < 7; i++){//led_array.numPixels(); i++) {
     for (int pixel_index=0; pixel_index < led_array.numPixels(); pixel_index++) {
       if (pixel_index <= i){
         led_array.setPixelColor(pixel_index, led_array.Color(4, 4, 4));
@@ -60,6 +86,47 @@ void setup() {
   }
 }
 
-// the loop function runs over and over again forever
 void loop() {
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
+  Serial.print(" since midnight 1/1/1970 = ");
+  Serial.print(now.unixtime());
+  Serial.print("s = ");
+  Serial.print(now.unixtime() / 86400L);
+  Serial.println("d");
+
+  // calculate a date which is 7 days and 30 seconds into the future
+  DateTime future (now + TimeSpan(7,12,30,6));
+
+  Serial.print(" now + 7d + 30s: ");
+  Serial.print(future.year(), DEC);
+  Serial.print('/');
+  Serial.print(future.month(), DEC);
+  Serial.print('/');
+  Serial.print(future.day(), DEC);
+  Serial.print(' ');
+  Serial.print(future.hour(), DEC);
+  Serial.print(':');
+  Serial.print(future.minute(), DEC);
+  Serial.print(':');
+  Serial.print(future.second(), DEC);
+  Serial.println();
+
+  Serial.println();
+  delay(3000);
 }
